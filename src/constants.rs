0218ad4488da49d6a43543238db7aec95337fa5c
Nicola Ricciardi <ricciardincl@gmail.com>
2024-12-14 09:51:03 +0100

    v1.0.1: fixed style

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const VERSION: &str = "0.40.0";
+pub const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
 
 pub const DOSSIER_CONFIGURATION_YAML_FILE_NAME: &str = "nmd.yml";
 pub const DOSSIER_CONFIGURATION_JSON_FILE_NAME: &str = "nmd.json";
