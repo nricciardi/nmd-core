d410892506df39aab3f494af9b6692a58f805ead
Nicola Ricciardi <ricciardincl@gmail.com>
2025-03-06 18:24:46 +0100

    Step

--- src/base_parameter.rs
+++ src/base_parameter.rs
@@ -4,11 +4,33 @@ pub mod effort;
 pub mod theme;
 
 
+use core::fmt;
+
+use delegate::delegate;
 use effort::Effort;
 use getset::{Getters, Setters};
 use output_format::OutputFormat;
 use parallelization::Parallelization;
 use theme::Theme;
+
+pub trait HasBaseConfiguration: fmt::Debug + Default {
+
+    fn output_format(&self) -> &OutputFormat;
+
+    fn set_output_format(&mut self, value: OutputFormat);
+
+    fn parallelization(&self) -> &Parallelization;
+
+    fn set_parallelization(&mut self, value: Parallelization);
+
+    fn effort(&self) -> &Effort;
+
+    fn set_effort(&mut self, value: Effort);
+
+    fn theme(&self) -> &Theme;
+
+    fn set_theme(&mut self, value: Theme);
+}
 
 
 #[derive(Debug, Default, Getters, Setters)]
@@ -25,5 +47,31 @@ pub struct BaseConfiguration {
 
     #[getset(get = "pub", set = "pub")]
     theme: Theme,
+}
+
+
+impl HasBaseConfiguration for BaseConfiguration {
+    
+    delegate! {
+        to self.output_format {
+            fn output_format(&self) -> &OutputFormat;
+            fn set_output_format(&mut self, value: OutputFormat);
+        }
+
+        to self.output_format {
+            fn parallelization(&self) -> &Parallelization;
+            fn set_parallelization(&mut self, value: Parallelization);
+        }
+
+        to self.effort {
+            fn effort(&self) -> &Effort;
+            fn set_effort(&mut self, value: Effort);
+        }
+
+        to self.theme {
+            fn theme(&self) -> &Theme;
+            fn set_theme(&mut self, value: Theme);
+        }
+    }
 }
 
